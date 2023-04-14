0d30b2fe39ac7c00d0f70eeae411c0637d2f852f
HaiwangYu <yuhw.pku@gmail.com>
2023-04-14 11:23:57 -0400

    pioneer:april2023

--- container/run_singularity.sh
+++ container/run_singularity.sh
@@ -1,7 +1,9 @@
-SIF=pioneer_ffdb71.sif # latest
+SIF=pioneer_april2023.sif
+#SIF=pioneer_ffdb71.sif # latest
 #SIF=pioneer_40c633.sif # test
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+echo $SCRIPT_DIR
 
 singularity exec \
   --bind /home \
